474241798f55bd5078f621203bdec007bf31b172
antew <antewcode@gmail.com>
2020-02-03 00:41:49 -0500

    Hook up purges

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,6 +1,30 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: [
+    "../**/*.html.eex",
+    "../**/*.html.leex",
+    "./js/**/*.js",
+    "../**/live/*.ex",
+    "../**/templates/*.ex"
+  ],
+  css: ["./css/app.css"],
+  extractors: [
+    {
+      extractor: content => {
+        let result = content.match(/[A-Za-z0-9-_:/]+/g) || [];
+        console.log(result);
+        return result;
+      },
+      extensions: ["leex", "eex", "js", "ex"]
+    }
+  ]
+});
+
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer')
+    require("postcss-import")(),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    purgecss
+    // ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
-}
+};
