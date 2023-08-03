62483d115d9c8e01f83dbd1f2351c99ac3feebf0
vtm9 <vtmilyakov@gmail.com>
2023-08-03 09:50:56 +0300

    Fix formatter

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule PlanningPoker.MixProject do
       version: "0.1.1",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
@@ -42,7 +42,6 @@ defmodule PlanningPoker.MixProject do
     [
       {:phoenix, "~> 1.7.7"},
       {:phoenix_pubsub, "~> 2.0"},
-      {:postgrex, "~> 0.17"},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.3", only: :dev},
       {:gettext, "~> 0.18"},
@@ -50,6 +49,7 @@ defmodule PlanningPoker.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:phoenix_live_view, "~> 0.19.5"},
       {:phoenix_live_dashboard, "~> 0.8"},
+      {:phoenix_view, "~> 2.0"},
       {:telemetry_poller, "~> 1.0"},
       {:telemetry_metrics, "~> 0.6"}
     ]
