63a47b71df29cf82ceeb4fa4426da921491e3b56
vtm9 <vtmilyakov@gmail.com>
2023-08-03 08:59:00 +0300

    Fix warnings

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@
 # is restricted to this project.
 
 # General application configuration
-use Mix.Config
+import Config
 
 config :planning_poker,
   ecto_repos: [PlanningPoker.Repo],
--- config/dev.exs
+++ config/dev.exs
@@ -1,4 +1,4 @@
-use Mix.Config
+import Config
 
 # Configure your database
 config :planning_poker, PlanningPoker.Repo,
